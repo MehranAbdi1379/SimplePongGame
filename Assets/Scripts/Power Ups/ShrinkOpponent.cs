bc4a3c77c8a32cc5b650cc46ce1d91c0af5df663
Mehran Abdi <mehranabdi13793@gmail.com>
2025-04-20 11:27:51 +0330

    Add shrink opponent power up

---
+++ Assets/Scripts/Power Ups/ShrinkOpponent.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+namespace Power_Ups
+{
+    public class ShrinkOpponent : PowerUp
+    {
+        private readonly float shrinkOpponentAmount;
+
+        public ShrinkOpponent()
+        {
+            shrinkOpponentAmount = 2;
+            PowerUpCoolDownTime = 4f;
+            PowerUpCoolDownCounter = PowerUpCoolDownTime;
+        }
+
+        protected override void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Ball") && other.gameObject.GetComponent<Rigidbody2D>().velocity.x > 0)
+            {
+                PowerUpActive = true;
+                var opponent = FindObjectOfType<AIPaddleController>().gameObject;
+                opponent.transform.localScale = new Vector3(opponent.transform.localScale.x,
+                    opponent.transform.localScale.y / shrinkOpponentAmount, opponent.transform.localScale.z);
+                var ballController = ball.GetComponent<BallController>();
+                ballController.currentBallState = BallController.BallState.PowerUp;
+                gameObject.transform.position = new Vector3(-20, -20, 0);
+                PowerUpCoolDownCounter = PowerUpCoolDownTime;
+            }
+        } // ReSharper disable Unity.PerformanceAnalysis
+        protected override void ReversePowerUpEffect()
+        {
+            var opponent = FindObjectOfType<AIPaddleController>().gameObject;
+            opponent.transform.localScale = new Vector3(opponent.transform.localScale.x,
+                opponent.transform.localScale.y * shrinkOpponentAmount, opponent.transform.localScale.z);
+
+            PowerUpActive = false;
+            ResetPowerUpCreateCoolDownCounter();
+        }
+    }
+}
